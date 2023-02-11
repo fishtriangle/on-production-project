5cfa95370a7d0c399230808dc4f8e48a4113dea9
fishtriangle <66234622+fishtriangle@users.noreply.github.com>
2023-02-11 12:08:01 +1200

    add es stylelint, i18next eslint plugin, fix scss errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:i18next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,6 +19,7 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'i18next',
   ],
   rules: {
     indent: [2, 2],
@@ -42,6 +44,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'no-underscore-dangle': 'off',
     'no-shadow': 'off',
+    'i18next/no-literal-string': ['error', { markupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
