2d7066049d007888687f5047ae3ab772bb358038
fishtriangle <66234622+fishtriangle@users.noreply.github.com>
2023-05-14 19:11:11 +1200

    add vite for dev mode

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API_BASE__: JSON.stringify('http://localhost:8888'),
+    __PROJECT__: JSON.stringify('frontend'),
+  },
+  css: {
+    modules: {
+      generateScopedName: '[path][name]__[local]--[hash:base64:5]',
+    },
+  },
+});
